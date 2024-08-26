49b440b043eb7c59a2a099d732f39624f0641b22
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2024-08-26 19:37:44 -0300

    criação de bases e dados no banco de dadaos

--- Cadastro_de_Fornecedores/app_cadastro_fornecedores/models.py
+++ Cadastro_de_Fornecedores/app_cadastro_fornecedores/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Fornecedores(models.Model):
+class Fornecedor(models.Model):
     id_fornecedor = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     cnpj = models.IntegerField()
     endereco = models.TextField(max_length=500)
-    celular = models.IntegerField
+    celular = models.IntegerField()
     email = models.TextField(max_length=255)
     produto = models.TextField(max_length=255)
     
--- Cadastro_de_Fornecedores/app_cadastro_fornecedores/views.py
+++ Cadastro_de_Fornecedores/app_cadastro_fornecedores/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Fornecedor
 
 # Create your views here.
 
@@ -6,4 +7,18 @@ def home(request):
     return render(request, "fornecedores/home.html")
 
 def fornecedores(request):
-    pass
+    #salvando dados no banco de dados
+    novo_fornecedor = Fornecedor()
+    novo_fornecedor.nome = request.POST.get('nome')
+    novo_fornecedor.cnpj = request.POST.get('cnpj')
+    novo_fornecedor.endereco = request.POST.get('endereco')
+    novo_fornecedor.celular = request.POST.get('celular')
+    novo_fornecedor.email = request.POST.get('email')
+    novo_fornecedor.produto = request.POST.get('produto')
+    novo_fornecedor.save()
+    #mostrando em nova pagina
+    fornecedores = {
+        'fornecedores': Fornecedor.objects.all()
+    }
+    #retornar os dados
+    return render(request, 'fornecedores/perfil.html',fornecedores)
